b21cdbb14f79c52a83f6d4bc9662d6fe77170194
gustavo <gustavo.maia@b2b.com.pt>
2020-05-04 13:18:16 +0100

    Listing Security Providers

---
+++ src/main/kotlin/Providers.kt
@@ -0,0 +1,39 @@
+import java.security.Provider
+import java.security.Security
+
+class Providers {
+
+    fun run(){
+        listAllProviders()
+    }
+
+
+    private fun listAllProviders(){
+
+        getProviders().forEach{
+            display(it)
+        }
+    }
+
+
+    private fun display(provider: Provider) {
+        println(provider.name)
+
+        println("------------------------------------------------------------------------------------------")
+
+        provider.entries.forEach { entry ->
+            println("\t ${entry.key}, ${entry.value}")
+        }
+
+        println("------------------------------------------------------------------------------------------")
+
+    }
+
+
+    private fun getProviders(): List<Provider> {
+
+        val providers = Security.getProviders()
+        val list: List<Provider> = providers.asList()
+        return list
+    }
+}
